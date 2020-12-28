039ff849a1bc6cf4ed25e26955bc3b0ffe99c653
tanaya123 <tanayawankar58@gmail.com>
2020-12-28 17:22:00 +0530

    Refactor Navbar styles and remove css

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,6 +7,78 @@ import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+
+
+
+import styles from './styles/NavBarStyles';
+
+
+// const styles={
+//     Navbar:{
+//         display: "flex",
+//         alignItems: "center",
+//         justifyContent: "flex-start",
+//         height: "6vh",
+    
+//     },
+    
+//     logo:{
+//         marginRight: "15px",
+//         padding: "0 13px",
+//         fontSize: "22px",
+//         backgroundColor: "#eceff1",
+//         fontFamily: "Roboto",
+//         height: "100%",
+//         display: "flex",
+//         alignItems: "center",
+//         "& a":{
+//             textDecoration: "none",
+//             color: "black",
+//         }
+    
+//     },
+    
+    
+    
+    
+//     slider :{
+//         width: "350px",
+//         margin: "0 10px",
+//         display: "inline-block",
+
+//         "& rc-slider-track":{
+//             backgroundColor: "transparent",
+//         },
+        
+//         "& rc-slider-rail":{
+//             height: "8px",
+//         },
+        
+//         "& rc-slider-handle,rc-slider-handle:active,rc-slider-handle:hover,rc-slider-handle:focus":{
+//             backgroundColor: "green",
+//             outline: "none",
+//             border:"2px solid green",
+//             boxShadow: "none",
+//             width:"13px",
+//             height: "13px",
+//             marginLeft: "-7px",
+//             marginTop: "-3px",
+        
+//         },
+        
+        
+        
+//     },
+//     selectContainer:{
+//         marginLeft: "auto",
+//         marginRight: "1rem",
+//     }
+
+    
+    
+// }
+
 
 class Navbar extends Component{
 
@@ -36,23 +108,24 @@ class Navbar extends Component{
 
     render()
     {
+        const {classes} = this.props;
 
         return(
-            <header className="Navbar">
-                <div className="logo">
+            <header className={classes.Navbar}>
+                <div className={classes.logo}>
                     <Link to="/">reactcolorpicker</Link>
                 </div>
                 
                 {this.props.showAllColors && (
                 <div className="slider-container">
                     <span>Level :{this.props.level}</span>
-                <div className="slider">
+                <div className={classes.slider}>
                     <Slider  defaultValue={this.props.level} step={100} min={100} max={900} onAfterChange={this.props.changeLevel}/>
                     </div>
                 </div>
                 )}
 
-                <div className="select-container">
+                <div className={classes.selectContainer}>
                     <Select value={this.state.format} onChange={this.handleChange}>
                         <MenuItem value="hex">HEX- #ffffff</MenuItem>
                         <MenuItem value="rgb">RGB-rgb(255,255,255)</MenuItem>
@@ -84,4 +157,4 @@ class Navbar extends Component{
 
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
