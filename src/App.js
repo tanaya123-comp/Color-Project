8c656c8ac82383c1946712a0fe6161cc587cd060
tanaya123 <tanayawankar58@gmail.com>
2021-01-04 18:00:25 +0530

    cleanup animation route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
@@ -6,11 +5,11 @@ import Pallete from './Pallete';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import {Switch,Route} from 'react-router-dom';
-import { Palette } from '@material-ui/icons';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
 import NewPaletteForm from './NewPaletteForm';
-import {TransitionGroup,CSSTransition} from "react-transition-group";
+import {TransitionGroup,CSSTransition} from 'react-transition-group';
+import Page from './Page';
 
 class App extends Component{
 
@@ -60,19 +59,19 @@ class App extends Component{
        
 
       return(
-        <Route render={({location})=>{
+        <Route render={({location})=>(
           <TransitionGroup>
-          <CSSTransition key={location.key} classNames="fade" timeout={500}>
+            <CSSTransition key={location.key} classNames="page" timeout={500}>
           <Switch location={location}>
           <Route
            exact path='/palette/new' 
            render={(routeProps)=> 
-           <div className="page">
+           <Page>
            <NewPaletteForm savePalette={this.savePalette} 
            palettes={this.state.palettes} 
            {...routeProps}
            />
-           </div>
+           </Page>
            }
 
            />
@@ -87,28 +86,28 @@ class App extends Component{
           <Route 
            exact path="/palette/:id"
             render={(routeProps)=>
-            <div className="page">
+            <Page>
             <Pallete 
             palette={generatePalette(this.findPalette(routeProps.match.params.id))}
             />
-          </div>
+          </Page>
           }
             />
           <Route
            exact path="/palette/:paletteId/:colorId" 
             render={(routeProps)=>
-           <div className="page">
+           <Page>
             <SingleColorPalette
              colorId={routeProps.match.params.colorId} 
              palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
              />
-             </div>
+             </Page>
              }
               />
         </Switch>
-        </CSSTransition>
+       </CSSTransition>
         </TransitionGroup>
-        }}
+        )}
         />
          
 
--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -60,6 +60,7 @@ class PaletteMetaForm extends Component{
               }
 
               this.props.handleSubmit(newPalette);
+              this.setState({stage:""});
           }
 
           
